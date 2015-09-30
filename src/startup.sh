e84b36d0dc960ed20581ad11563dadaaf2ad94b9
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-09-30 11:32:48 +0100

    launch epiphany-browser on start with xinit

--- src/startup.sh
+++ src/startup.sh
@@ -1,3 +1,18 @@
+#/bin/bash
+
+# Disable DPMS / Screen blanking
+ xset -dpms
+ xset s off
+ xset s noblank
+
+mkdir /root/.config
+url=$URL
+default='http://www.noiseaddicts.com/samples_1w72b820/3712.mp3'
+sudo matchbox-window-manager -use_cursor no -use_titlebar no &
+xte 'sleep 15' 'key F11'&
+epiphany-browser -a --profile /root/.config ${url:-$default} --display=:0
+sleep 2s
+
 while [ 1 ]
 do
     echo "waiting.."
